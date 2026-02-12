7808f372deb12d07107cd89e09ee94c05a8b42d6
Nestor Mosquera <revoluc.global@gmail.com>
2026-02-12 10:25:36 -0500

    chore(business-logic): add roles, membership and workspaces logic

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -7,11 +7,7 @@ export default {
     'footer-max-line-length': [2, 'always', 100],
     'header-max-length': [2, 'always', 100],
     'scope-case': [2, 'always', 'lower-case'],
-    'subject-case': [
-      2,
-      'never',
-      ['sentence-case', 'start-case', 'pascal-case', 'upper-case'],
-    ],
+    'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
     'subject-empty': [2, 'never'],
     'subject-full-stop': [2, 'never', '.'],
     'type-case': [2, 'always', 'lower-case'],
@@ -56,9 +52,10 @@ export default {
         'infra',
         'k8s',
         'terraform',
-        '{{packageName}}',
-        'deps'
-      ]
+        'business-logic',
+        'contracts',
+        'deps',
+      ],
     ],
   },
 };
