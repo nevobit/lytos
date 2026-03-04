e27755e1247c2c5bf8b5ebea6c1b60d5805314e1
nestor <nevobit.global@gmail.com>
2026-03-04 17:35:56 -0500

    chore: portal deploy

--- apps/portal/src/sw/types.d.ts
+++ apps/portal/src/sw/types.d.ts
@@ -6,4 +6,8 @@ interface ServiceWorkerGlobalScope {
     onperiodicsync: ((this: ServiceWorkerGlobalScope, ev: PeriodicSyncEvent) => unknown) | null;
 }
 
+interface ServiceWorkerGlobalScopeEventMap {
+    periodicsync: PeriodicSyncEvent;
+}
+
 declare const self: ServiceWorkerGlobalScope;
