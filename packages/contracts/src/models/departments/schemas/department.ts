f9b3a7355abd2496ebb26c4469fbaaa527829138
nestor <nevobit.global@gmail.com>
2026-03-06 16:24:58 -0500

    chore: departments crud

--- packages/contracts/src/models/departments/schemas/department.ts
+++ packages/contracts/src/models/departments/schemas/department.ts
@@ -1,5 +1,5 @@
 import { type Base } from '../../../common';
-import { User } from '../../users';
+import type { User } from '../../users';
 
 export interface Department extends Base {
     workspaceId: string;
