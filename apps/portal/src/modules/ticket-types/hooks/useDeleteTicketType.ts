2d7c11033484b6856e863f06bab996473c02be5f
nestor <nevobit.global@gmail.com>
2026-03-08 20:17:21 -0500

    chore: lytos general workspace

--- apps/portal/src/modules/ticket-types/hooks/useDeleteTicketType.ts
+++ apps/portal/src/modules/ticket-types/hooks/useDeleteTicketType.ts
@@ -3,12 +3,12 @@ import { deleteTicketType } from "../services";
 
 export const useDeleteTicketType = () => {
     const queryClient = useQueryClient();
-    const { mutateAsync, isLoading, error } = useMutation({
+    const { mutateAsync, isPending, error } = useMutation({
         mutationFn: deleteTicketType,
         onSuccess: async () => {
             await queryClient.invalidateQueries({ queryKey: ['ticketTypes'] });
         },
     });
 
-    return { remove: mutateAsync, isLoading, error };
+    return { remove: mutateAsync, isLoading: isPending, error };
 };
